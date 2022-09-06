2fbc45fa720f8284cd968a7b55d607e638569c17
mousQTR <mousqtr@gmail.com>
2022-09-06 12:20:01 +0000

    Update 3 files

---
+++ src/Profile/Profile.js
@@ -0,0 +1,10 @@
+import ProfileNav from "./ProfileNav/ProfileNav";
+import "./Profile.scss";
+
+export default function Profile() {
+  return (
+    <div id="profile" className="">
+      <ProfileNav />
+    </div>
+  );
+}
