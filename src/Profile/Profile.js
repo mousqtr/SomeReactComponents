b3d4e77cfad799ed25c9d8c1c88c31381bc6a3dc
mousQTR <mousqtr@gmail.com>
2022-09-07 15:38:43 +0000

    Update 4 files

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -1,10 +1,20 @@
+import React, { useState } from "react";
 import ProfileNav from "./ProfileNav/ProfileNav";
+import { GiHamburgerMenu } from "react-icons/gi";
 import "./Profile.scss";
 
 export default function Profile() {
+  
+  const [openNav, setOpenNav] = useState(true);
+  
+  const handleOpen = () => setOpenNav(true);
+  
   return (
     <div id="profile" className="">
-      <ProfileNav />
+        <div className="profileTitle center" onClick={handleOpen}>
+            <GiHamburgerMenu />
+        </div>
+      <ProfileNav openNav={openNav} setOpenNav={setOpenNav}/>
     </div>
   );
 }
