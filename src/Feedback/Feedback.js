d9f70ff9319eeb974797660bdba0550dcef86090
mousQTR <mousqtr@gmail.com>
2022-09-12 16:14:34 +0000

    Update 7 files

--- src/Feedback/Feedback.js
+++ src/Feedback/Feedback.js
@@ -1,10 +1,12 @@
+import FeedbackHeader from "./FeedbackHeader/FeedbackHeader";
 import FeedbackForm from "./FeedbackForm/FeedbackForm";
 import "./Feedback.scss";
 
 export default function Feedback() {
   return (
-    <div id="feedback" className="section center">
-      <FeedbackForm />
+    <div id="feedback" className="section-screen">
+        <FeedbackHeader /> 
+        <FeedbackForm />
     </div>
   );
 }
