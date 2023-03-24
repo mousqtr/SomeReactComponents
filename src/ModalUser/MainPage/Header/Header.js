647fe77c12a1643b0c33b574d195d3490ff6cd3c
mousQTR <mousqtr@gmail.com>
2023-03-24 13:46:21 +0000

    Update Header.js and Image.js

--- src/ModalUser/MainPage/Header/Header.js
+++ src/ModalUser/MainPage/Header/Header.js
@@ -46,12 +46,18 @@ export default function Header({ mode }) {
           d="M0,256L80,229.3C160,203,320,149,480,160C640,171,800,245,960,240C1120,235,1280,149,1360,106.7L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
         ></path>
       </svg>
-      <div
-        className="edit-header center"
-        onClick={() => setColorBg(!isColorBg)}
-      >
-        <BiPaint type="color" />
-      </div>
+
+      {mode === "edit" ? (
+        <div
+          className="edit-header center"
+          onClick={() => setColorBg(!isColorBg)}
+        >
+          <BiPaint type="color" />
+        </div>
+      ) : (
+        <></>
+      )}
+
       {isColorBg ? (
         <ColorBg
           colors={colors}
