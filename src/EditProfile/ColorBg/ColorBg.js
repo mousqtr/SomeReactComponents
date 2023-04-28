cb4bba3e51ff27dce8e75935aed91e2ef7817b9f
mousQTR <mousqtr@gmail.com>
2023-04-28 11:29:21 +0000

    Update 4 files

--- src/EditProfile/ColorBg/ColorBg.js
+++ src/EditProfile/ColorBg/ColorBg.js
@@ -5,45 +5,35 @@ import { ImCross } from "react-icons/im";
 import "./ColorBg.scss";
 
 export default function ({ colors, setColors, colorsToString, close }) {
-  const [localColors, setLocalColors] = useState(colors);
-  const [displayedColors, setDisplayedColors] = useState(
-    colorsToString(localColors)
-  );
-
-  const handleSetLocalColor = (e, index) => {
-    const localColors_ = [...localColors];
-    localColors_[index].color = e.target.value;
-    setLocalColors(localColors_);
+  const handleSetColor = (e, index) => {
+    const colors_ = [...colors];
+    colors_[index].color = e.target.value;
+    setColors(colors_);
   };
 
-  const handleSetLocalStop = (e, index) => {
-    const localColors_ = [...localColors];
-    localColors_[index].stop = e.target.value;
-    setLocalColors(localColors_);
+  const handleSetStop = (e, index) => {
+    const colors_ = [...colors];
+    colors_[index].stop = e.target.value;
+    setColors(colors_);
   };
 
   const handleRemove = (index) => {
-    let localColors_ = [...localColors];
-    localColors_.splice(index, 1);
-    if (localColors_.length === 1) {
-      localColors_[0].stop = 100;
+    let colors_ = [...colors];
+    colors_.splice(index, 1);
+    if (colors_.length === 1) {
+      colors_[0].stop = 100;
     }
-    setLocalColors(localColors_);
+    setColors(colors_);
   };
 
   const handleAdd = () => {
-    const localColors_ = [
-      ...localColors,
+    setColors([
+      ...colors,
       {
         color: "#000000",
         stop: "100",
       },
-    ];
-    setLocalColors(localColors_);
-  };
-
-  const handleSave = () => {
-    setColors(localColors);
+    ]);
   };
 
   const handleClose = () => {
@@ -54,52 +44,48 @@ export default function ({ colors, setColors, colorsToString, close }) {
     <div id="colorBg">
       <div
         className="preview"
-        style={{ background: colorsToString(localColors) }}
+        style={{ background: colorsToString(colors) }}
       ></div>
 
-      {localColors.map((elt, index) => (
-        <div key={index} className="sub-block bothColor center-row">
-          <input
-            type="color"
-            className="color"
-            value={elt.color}
-            onChange={(e) => handleSetLocalColor(e, index)}
-          />
-          <input
-            type="number"
-            className="stop"
-            disabled={localColors.length === 1 && index === 0}
-            min="0"
-            max="100"
-            value={elt.stop}
-            onChange={(e) => handleSetLocalStop(e, index)}
-          />
-
-          <div className="remove center">
-            <BiTrash
-              onClick={() => handleRemove(index)}
-              className={index === 0 ? "disabled" : ""}
+      <div className="sub-blocks">
+        {colors.map((elt, index) => (
+          <div key={index} className="sub-block bothColor center-row">
+            <input
+              type="color"
+              className="color"
+              value={elt.color}
+              onChange={(e) => handleSetColor(e, index)}
             />
+            <input
+              type="number"
+              className="stop"
+              disabled={colors.length === 1 && index === 0}
+              min="0"
+              max="100"
+              value={elt.stop}
+              onChange={(e) => handleSetStop(e, index)}
+            />
+
+            <div className="remove center">
+              <BiTrash
+                onClick={() => handleRemove(index)}
+                className={index === 0 ? "disabled" : ""}
+              />
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
 
-      {localColors.length === 1 ? (
-        <div className="sub-block bothColor center-row add" onClick={handleAdd}>
-          <BsPlusCircleDotted />
-          <span>Ajouter couleur</span>
-        </div>
-      ) : (
-        <></>
-      )}
-
-      <div className="bottom-block center-row">
-        <div className="button button-cancel center" onClick={handleClose}>
-          <ImCross />
-        </div>
-        <div className="button button-save center" onClick={handleSave}>
-          <BsCheckLg />
-        </div>
+        {colors.length === 1 ? (
+          <div
+            className="sub-block bothColor center-row add"
+            onClick={handleAdd}
+          >
+            <BsPlusCircleDotted />
+            <span>Ajouter couleur</span>
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
